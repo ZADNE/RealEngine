f658c9f359c3bc7ef8de4df74797019b0ca73e6f
Tomáš Dubský <td.zadne@seznam.cz>
2024-09-03 22:09:38 +0200

    Reprint problems at the end of the step

--- .github/scripts/run-clang-tidy.py
+++ .github/scripts/run-clang-tidy.py
@@ -6,26 +6,30 @@ import os, sys, subprocess, json
 print("Arguments: " + str(sys.argv))
 clangTidyFilepath = sys.argv[1]
 jsonFilepath = sys.argv[2]
+outputFilepath = sys.argv[3]
+
+open(outputFilepath, 'a').close()
 
 # Parse JSON to dict
 json = json.load(open(jsonFilepath))
 
 # Runs clang-tidy on one entry
-# Return 1 if failed, 0 otherwise
 def runClangTidy(entry):
     filepath = entry['file']
-    print("Checking: " + filepath)
+    print("Checking: " + filepath, flush=True)
     command = entry['command']
-    proc = subprocess.Popen(f'{clangTidyFilepath} {filepath} -- {command}', shell=True)
-    return proc.wait() != 0
+    proc = subprocess.Popen(f'{clangTidyFilepath} --quiet {filepath} -- {command} 2>/dev/null | tee -a {outputFilepath}', shell=True)
+    proc.wait()
 
 # Check files
-failedCount = 0
 for entry in json:
-    failedCount += runClangTidy(entry)
+    runClangTidy(entry)
 
 # Report output
-if failedCount > 0:
-    print(f'{failedCount} files did not pass linting!')
+if os.stat(outputFilepath).st_size > 0:
+    print(f'Some files did not pass linting! Problems follow:')
+    log = open(outputFilepath, "r")
+    for line in log:
+        print(line)
     sys.exit(1)
 sys.exit(0)
