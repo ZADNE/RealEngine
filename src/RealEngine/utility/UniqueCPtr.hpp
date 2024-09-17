80a8920432d5bda263205a0ea6e7f005d092cd48
Tomáš Dubský <td.zadne@seznam.cz>
2024-09-17 20:33:05 +0200

    Create releases

--- src/RealEngine/utility/UniqueCPtr.hpp
+++ src/RealEngine/utility/UniqueCPtr.hpp
@@ -6,6 +6,19 @@
 
 namespace re {
 
+namespace details {
+/**
+ * @todo Use lambda instead of this once GCC in CI can handle it.
+ */
+template<auto DeleteFn>
+struct UniqueCPtrDeleter {
+    template<class T>
+    void operator()(T* ptr) const {
+        DeleteFn(ptr);
+    }
+};
+} // namespace details
+
 /**
  * @brief           Implements RAII for C-style pointer-to-opaque objects
  * @tparam T        The opaque type
@@ -13,6 +26,6 @@ namespace re {
  * @author          Andreas Fertig (C++ Insights: Lambdas in unevaluated contexts)
  */
 template<typename T, auto DeleteFn>
-using UniqueCPtr = std::unique_ptr<T, decltype([](T* obj) { DeleteFn(obj); })>;
+using UniqueCPtr = std::unique_ptr<T, details::UniqueCPtrDeleter<DeleteFn>>;
 
 } // namespace re
