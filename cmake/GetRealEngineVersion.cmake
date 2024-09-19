1af65967d5824c54f0bb182f02bfca0d9eed4918
Tomáš Dubský <td.zadne@seznam.cz>
2024-09-19 18:47:09 +0200

    Automatically fetch version of RealEngine

---
+++ cmake/GetRealEngineVersion.cmake
@@ -0,0 +1,19 @@
+# author     Dubsky Tomas
+
+function(get_realengine_version)
+    file(READ "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../src/RealEngine/utility/Version.hpp" version_file)
+    string(REGEX MATCH "constexpr int k_versionMajor = ([0-9]+)" _ ${version_file})
+    set(version_major "${CMAKE_MATCH_1}")
+    string(REGEX MATCH "constexpr int k_versionMinor = ([0-9]+)" _ ${version_file})
+    set(version_minor "${CMAKE_MATCH_1}")
+    string(REGEX MATCH "constexpr int k_versionPatch = ([0-9]+)" _ ${version_file})
+    set(version_patch "${CMAKE_MATCH_1}")
+
+    set(realengine_version "${version_major}.${version_minor}.${version_patch}" PARENT_SCOPE)
+endfunction()
+
+# Print the version to stdout if running in script mode
+if(DEFINED CMAKE_ARGV0)
+    get_realengine_version()
+    execute_process(COMMAND ${CMAKE_COMMAND} -E echo "${realengine_version}")
+endif()
--- src/RealEngine/window/WindowSubsystems.hpp
+++ src/RealEngine/window/WindowSubsystems.hpp
@@ -4,6 +4,8 @@
 #pragma once
 #include <string>
 
+#include <RealEngine/utility/Version.hpp>
+
 /*! \mainpage RealEngine
  *
  * \section overview Overview
@@ -19,16 +21,6 @@
  */
 
 namespace re {
-
-constexpr int k_versionMajor = 2;
-constexpr int k_versionMinor = 0;
-constexpr int k_versionPatch = 0;
-
-/**
- * @brief Is integer representation of RealEngine's version
- */
-constexpr int k_version = k_versionMajor * 1'000'000 + k_versionMinor * 1'000 +
-                          k_versionPatch;
 
 /**
  * @brief Lists all renderers known to RealEngine
