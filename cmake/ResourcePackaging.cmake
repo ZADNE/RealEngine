d9a62b27925597b108c533a4f674639213181f80
Tomáš Dubský <td.zadne@seznam.cz>
2025-02-19 21:52:14 +0100

    Install bit7z as a part of RealEngine

--- cmake/ResourcePackaging.cmake
+++ cmake/ResourcePackaging.cmake
@@ -1,30 +1,43 @@
-# author     Dubsky Tomas
+﻿# author     Dubsky Tomas
 
 # Create a custom target that will index and package files:
 # Syntax:
 #     real_target_package_resources(
 #        TARGET <target>
-#        OUTPUT_DIR <directory_name>
+#        PACKAGE_DIR <directory_name>
+#        INDEX_FILE <file_path>
 #        INPUT_DIRS <directory_name>...
 #     )
+# The index is a C++ header file containing re::ResourceIDs of the packaged files.
 # The target will be named <target>_PackageResources.
 # The packaging will be done in non-debug builds only as debug build of RealEngine
 # reads the unpackaged data.
 function(real_target_package_resources)
-    set(one_value_args TARGET OUTPUT_DIR)
+    set(one_value_args TARGET PACKAGE_DIR INDEX_FILE)
     set(multi_value_args INPUT_DIRS)
     cmake_parse_arguments(ARG "" "${one_value_args}" "${multi_value_args}" ${ARGN})
 
-    set(output_package "${ARG_OUTPUT_DIR}/package.dat")
-    get_target_property(realengine_binary_dir RealEngine BINARY_DIR)
+    set(output_package "${ARG_PACKAGE_DIR}/package.dat")
+    get_target_property(realengine_source_dir RealEngine HEADER_DIRS_realproject_public_headers)
 
+    # Copy dummy index which will be used for debug builds
+    if(NOT EXISTS ${ARG_INDEX_FILE})
+        configure_file(
+            "${realengine_source_dir}/RealEngine/resources/ResourceIndex.hpp"
+            ${ARG_INDEX_FILE}
+            COPYONLY
+        )
+    endif()
+
+    # Add the packaging command
     add_custom_command(
         OUTPUT ${output_package}
         COMMAND ResourcePackager
                     "$<LIST:TRANSFORM,${ARG_INPUT_DIRS},PREPEND,--in=>"
-                    -o ${ARG_OUTPUT_DIR}
-                    --index "${realengine_binary_dir}/src/RealEngine/resources/ResourceIndex.hpp"
+                    -o ${ARG_PACKAGE_DIR}
+                    --index ${ARG_INDEX_FILE}
         DEPENDS ResourcePackager
+        BYPRODUCTS ${ARG_INDEX_FILE}
         COMMENT "Packaging resources for ${ARG_TARGET}..."
         VERBATIM
         COMMAND_EXPAND_LISTS
